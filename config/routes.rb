94f62e5e9e987c17f5ed415f23e90fa8b21fc538
Victor <victorgmassimi.microverse@gmail.com>
2023-04-24 17:06:14 -0600

    devise setup complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'groups#index'
+
   resources :groups do
     resources :operations
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "groups#index"
 end
