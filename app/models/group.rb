1ae44796e2453c48e5dacd10f232a1a953cb4e86
Victor <victorgmassimi.microverse@gmail.com>
2023-04-26 15:03:20 -0600

    Added operations functionality

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,11 +5,22 @@ class Group < ApplicationRecord
 
   validates :name, presence: true
 
+  after_commit :add_default_image, on: %i[create update]
+
   def value
     value = 0
     operations&.each do |op|
       value += op.amount
     end
     value
+  end
+
+  private
+
+  def add_default_image
+    return if image.attached?
+
+    image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'placeholder-image.png')),
+                 filename: 'placeholder-image.png', content_type: 'image/png')
   end
 end
